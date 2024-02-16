afe01f37bf74e3af4fc775fd53be2b36900c9893
Sujit Roy <sujitroy78148@gmail.com>
2024-02-16 23:26:38 +0530

    createConnection to createPool in database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,21 +1,12 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL database:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
-
-export default db;
+export default pool;
