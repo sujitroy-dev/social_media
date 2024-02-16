97d4b0611b9ed30e81f055c9f4e9da7f0f1b2879
Sujit Roy <sujitroy78148@gmail.com>
2024-02-16 23:42:50 +0530

    setup swagger

---
+++ backend/src/config/swagger.js
@@ -0,0 +1,26 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: "PetPals",
+      version: "1.0.0",
+      description:
+        "PetPals is a community platform where you can upload photos and videos of your pets.",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+      },
+    ],
+    basePath: "/",
+  },
+  apis: ["src/routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+export { specs, swaggerUi };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import db from "./config/database.js";
+import { specs, swaggerUi } from "./config/swagger.js";
+import usersRouter from "./routes/users.route.js";
 const app = express();
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 
+// Serve Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 // Routes
-// app.use('/api/user', user)
+app.use("/api/users", usersRouter);
 
 const port = process.env.PORT || 8080;
 
