879cacc9dd0ddc9cd0ffff8b53d44237f1b32260
Sujit Roy <sujitroy78148@gmail.com>
2024-02-24 21:33:51 +0530

    updated pet api's

--- backend/src/controllers/pet.controller.js
+++ backend/src/controllers/pet.controller.js
@@ -1,44 +1,72 @@
 import pool from "../config/database.js";
 
 export const getPetsByUser = async (req, res) => {
+  const user_id = req.params.id;
+  let connection;
   try {
-    const user_id = parseInt(req.params.id);
-    const [rows] = await pool.query(
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+
+    const [rows] = await connection.query(
       `SELECT *
       FROM pet
       WHERE user_id = ?`,
       user_id
     );
 
+    await connection.commit();
     res.status(200).json(rows);
   } catch (error) {
+    if (connection) {
+      await connection.rollback();
+    }
     console.error(error);
     return res.status(500).json({ message: error.message });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
 
 export const getPetById = async (req, res) => {
+  const petID = req.params.id;
+  let connection;
   try {
-    const petID = parseInt(req.params.id);
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+
     const GET_QUERY = `SELECT * FROM pet WHERE id = ?`;
-    const [raws] = await pool.query(GET_QUERY, petID);
+    const [raws] = await connection.query(GET_QUERY, petID);
+
+    await connection.commit();
 
     Array.isArray(raws) && raws.length > 0 && res.status(200).send(raws[0]);
     return res.send({ message: "Not found" });
   } catch (error) {
+    if (connection) {
+      await connection.rollback();
+    }
     console.error(error);
     return res.status(500).json({ message: error.message });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
 
 export const createPet = async (req, res) => {
   let { user_id, name, breed, type, dob, about } = req.body;
-
-  const INSERT_QUERY = `INSERT INTO pet (user_id, name, breed, type, dob, about)
-  VALUES (?, ?, ?, ?, ?, ?)`;
+  let connection;
 
   try {
-    const response = await pool.query(INSERT_QUERY, [
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+
+    const INSERT_QUERY = `INSERT INTO pet (user_id, name, breed, type, dob, about)
+    VALUES (?, ?, ?, ?, ?, ?)`;
+    const response = await connection.query(INSERT_QUERY, [
       user_id,
       name,
       breed,
@@ -46,6 +74,8 @@ export const createPet = async (req, res) => {
       dob,
       about,
     ]);
+
+    await connection.commit();
 
     if (response?.[0].affectedRows > 0) {
       return res.status(201).json({
@@ -60,8 +90,15 @@ export const createPet = async (req, res) => {
     }
     return res.json({ message: "Missing or invalid parameters" });
   } catch (error) {
+    if (connection) {
+      connection.rollback();
+    }
     console.error(error);
     return res.status(500).json({ message: error.message });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
 
@@ -69,12 +106,15 @@ export const updatePet = async (req, res) => {
   let { id, user_id, name, breed, type, dob, about } = req.body;
   const petID = req.params.id;
   const newDate = { id, user_id, name, breed, type, dob, about };
-
-  const UDPATE_QUERY = `UPDATE pet SET ? WHERE id = ?`;
+  let connection;
 
   try {
-    const response = await pool.query(UDPATE_QUERY, [newDate, petID]);
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+    const UDPATE_QUERY = `UPDATE pet SET ? WHERE id = ?`;
+    const response = await connection.query(UDPATE_QUERY, [newDate, petID]);
 
+    connection.commit();
     if (response?.[0].affectedRows > 0) {
       return res.status(201).json({
         message: "Updated successfully",
@@ -83,24 +123,42 @@ export const updatePet = async (req, res) => {
     }
     return res.json({ message: "Missing or invalid parameters" });
   } catch (error) {
+    if (connection) {
+      connection.rollback();
+    }
     console.error(error);
     return res.status(500).json({ message: error.message });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
 
 export const deletePet = async (req, res) => {
+  const petID = req.params.id;
+  let connection;
   try {
-    const petID = parseInt(req.params.id);
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+
     const DELETE_QUERY = `DELETE FROM pet WHERE id = ?`;
+    const [response] = await connection.query(DELETE_QUERY, petID);
 
-    const [response] = await pool.query(DELETE_QUERY, petID);
-
+    connection.commit();
     if (response.affectedRows > 0) {
       return res.status(200).send({ message: "Removed successfully" });
     }
     return res.send({ message: "Failed to update, Invalid id" });
   } catch (error) {
+    if (connection) {
+      connection.rollback();
+    }
     console.error(error);
     return res.status(500).json({ message: error.message });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
