3adc05467ac0318bc74f6f9413d7c4efd40ac1e1
Sujit Roy <sujitroy78148@gmail.com>
2024-02-29 19:11:46 +0530

    updated next.config and added white_listed_image_url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
+// const webpack = require("webpack");
+import webpack from "webpack";
+import dotenv from "dotenv";
+const { parsed: myEnv } = dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: process.env.WHITE_LIST_IMAGE_URLS.split(","),
+  },
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
 
 export default nextConfig;
