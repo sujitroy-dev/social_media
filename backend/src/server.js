955122e1d40e7a5e620e26dfd980069c524bb765
Sujit Roy <sujitroy78148@gmail.com>
2024-03-01 20:55:04 +0530

    remove petsRouter from server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { specs, swaggerUi } from "./config/swagger.js";
 import usersRouter from "./routes/users.route.js";
-import petsRouter from "./routes/pet.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
@@ -19,7 +18,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Routes
 app.use("/api/users", usersRouter);
-app.use("/api/pet", petsRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 
