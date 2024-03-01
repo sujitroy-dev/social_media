6312f816274130edea9d36203677876c985a408b
Sujit Roy <sujitroy78148@gmail.com>
2024-03-01 19:17:27 +0530

    use post card and create post card in feeds page

--- frontend/app/feed/page.jsx
+++ frontend/app/feed/page.jsx
@@ -1,14 +1,15 @@
 "use client";
 import AuthForm from "@/components/auth";
-import Post from "@/components/post";
+import Post from "@/components/Feed/post";
 import MainLayout from "@/layouts/main";
 import { Typography } from "@material-tailwind/react";
 import { useState } from "react";
-import posts from "../../data/posts.json";
-import CreatePost from "@/components/createPost";
+import postsArr from "../../data/posts.json";
+import CreatePost from "@/components/Feed/createPost";
 
 export default function Home() {
   const [showAuthForm, SetShowAuthForm] = useState(false);
+  const [posts, setPosts] = useState(postsArr || []);
 
   return (
     <>
@@ -18,11 +19,11 @@ export default function Home() {
       />
       <MainLayout>
         <CreatePost />
-        <div>
+        <section aria-label="Feed Posts">
           {posts.map((post) => (
             <Post {...post} />
           ))}
-        </div>
+        </section>
       </MainLayout>
     </>
   );
