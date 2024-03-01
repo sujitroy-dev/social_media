b6e59238550eb5d1e071f17cccb6bfab0a56f8b7
Sujit Roy <sujitroy78148@gmail.com>
2024-03-01 19:17:08 +0530

    create new post component created

---
+++ frontend/components/Feed/createPost.jsx
@@ -0,0 +1,54 @@
+import { PhotoIcon } from "@heroicons/react/24/outline";
+import { Avatar, Button, IconButton, Tooltip } from "@material-tailwind/react";
+import React, { useState } from "react";
+import NameAvatar from "../NameAvatar";
+
+export default function CreatePost() {
+  const [text, setText] = useState("");
+  const [isInputFocus, setIsInputFocus] = useState(false);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+    event.target.style.height = "auto";
+    event.target.style.height = event.target.scrollHeight + "px";
+  };
+
+  return (
+    <section className="border-x border-t border-gray-200 p-8 bg-gray-50 rounded-t-sm">
+      <div
+        className={`p-2 rounded-md bg-white mb-4 border ${isInputFocus ? "border-gray-500 shadow-sm" : "border-gray-300"} flex gap-2`}
+      >
+        {"valid" ? (
+          <Avatar
+            src="https://docs.material-tailwind.com/img/face-2.jpg"
+            alt="avatar"
+            size="md"
+            variant="rounded"
+          />
+        ) : (
+          <NameAvatar name="sujit roy" rounded="md" />
+        )}
+
+        <textarea
+          class="block w-full text-base resize-none focus:outline-none  no-scrollbar flex-1"
+          placeholder="Share something..."
+          onChange={handleTextChange}
+          onFocus={() => setIsInputFocus(true)}
+          onBlur={() => setIsInputFocus(false)}
+        >
+          {text}
+        </textarea>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="">
+          <Tooltip content="Select Photos" placement="bottom">
+            <IconButton variant="text" className="rounded-full">
+              <PhotoIcon color="black" width={25} height={25} />
+            </IconButton>
+          </Tooltip>
+        </div>
+        <Button className="rounded-full">Send</Button>
+      </div>
+    </section>
+  );
+}
