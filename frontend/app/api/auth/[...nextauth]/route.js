e61927678108961e6949390b68d32bba70ca974b
Sujit Roy <sujitroy78148@gmail.com>
2024-03-05 23:42:17 +0530

    auth config

---
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log({
+  googleID: process.env.GOOGLE_CLIENT_ID,
+  googleSec: process.env.GOOGLE_SECRET_ID,
+  twitterID: process.env.TWITTER_CLIENT_ID,
+  twitterSec: process.env.TWITTER_CLIENT_SECRET,
+});
+
+if (
+  !process.env.GOOGLE_CLIENT_ID ||
+  !process.env.GOOGLE_SECRET_ID ||
+  !process.env.TWITTER_CLIENT_ID ||
+  !process.env.TWITTER_CLIENT_SECRET
+) {
+  throw new Error("Auth required env variables are not set");
+}
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
