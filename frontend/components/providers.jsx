1205f0a28614f899c84e1421f64cf732a1374a7f
Sujit Roy <sujitroy78148@gmail.com>
2024-03-05 23:48:40 +0530

    next-auth provider added on providers component

--- frontend/components/providers.jsx
+++ frontend/components/providers.jsx
@@ -1,12 +1,15 @@
 "use client";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "@/store";
 
 export function Providers({ children }) {
   return (
-    <ReduxProvider store={store}>
-      <ThemeProvider>{children}</ThemeProvider>
-    </ReduxProvider>
+    <SessionProvider>
+      <ReduxProvider store={store}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </ReduxProvider>
+    </SessionProvider>
   );
 }
