94d9e57196b5e04074d59ea81d6315dc6dd051e6
Sujit Roy <sujitroy78148@gmail.com>
2024-03-05 23:51:27 +0530

    remove userSaga from root sagag

--- frontend/store/sagas/rootSaga.js
+++ frontend/store/sagas/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from "redux-saga/effects";
-import { userSaga } from "./userSaga";
 import { postsSaga } from "./postsSaga";
 
 export default function* rootSaga() {
-  yield all([userSaga(), postsSaga()]);
+  yield all([postsSaga()]);
 }
