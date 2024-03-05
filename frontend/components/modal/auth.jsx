ac7d5c8fb1a5c068af73e4f92567e6e3e8c0637e
Sujit Roy <sujitroy78148@gmail.com>
2024-03-05 23:42:51 +0530

    auth modal moved to components/modal

---
+++ frontend/components/modal/auth.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { Button, Dialog, Typography, Input } from "@material-tailwind/react";
+import React, { useState } from "react";
+import Icon from "../icons";
+import { useSelector } from "react-redux";
+import { signIn } from "next-auth/react";
+
+export default function AuthForm({ closeModal }) {
+  const isModalOpen = !useSelector((state) => state.user.loggedIn);
+
+  return (
+    <Dialog open={isModalOpen} className="p-3 w-full" handler={closeModal}>
+      <div className="w-full flex flex-col items-center justify-center my-5">
+        <div className="text-center">
+          <Typography variant="h2" className="font-bold mb-3">
+            Sign in
+          </Typography>
+          <Typography
+            variant="paragraph"
+            color="blue-gray"
+            className="text-lg font-normal"
+          >
+            Sign in with provided options.
+          </Typography>
+        </div>
+        <div className="mt-2 mb-2 mx-auto max-w-screen-lg w-2/3">
+          <div className="space-y-4 my-4">
+            <LoginOptions />
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+
+function LoginOptions() {
+  return (
+    <>
+      <Button
+        size="lg"
+        color="white"
+        className="flex items-center gap-2 justify-center shadow-md"
+        fullWidth
+        onClick={() => signIn("google", { callbackUrl: "/feed" })}
+      >
+        <Icon.Google />
+        <span>Sign in With Google</span>
+      </Button>
+      <Button
+        size="lg"
+        color="white"
+        className="flex items-center gap-2 justify-center shadow-md"
+        fullWidth
+        onClick={() => signIn("twitter", { callbackUrl: "/feed" })}
+      >
+        <Icon.Twitter />
+        <span>Sign in With Twitter</span>
+      </Button>
+    </>
+  );
+}
